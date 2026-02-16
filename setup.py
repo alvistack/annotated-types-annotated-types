5b947940efcba497bba61c1174b14810ae5f6aa0
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-16 10:46:16 +0800

    alvistack/v0.7.0
        git clean -xdf
        tar zcvf ../python-annotated-types_0.7.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-annotated-types.spec ../python-annotated-types_0.7.0-1.spec
        cp ../python*-annotated-types*0.7.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/annotated-types-annotated-types-0.7.0/
        rm -rf ../python*-annotated-types*0.7.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='annotated-types',
+    version='0.7.0',
+    description='Reusable constraint types to use with typing.Annotated',
+    author_email='Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>, Samuel Colvin <s@muelcolvin.com>, Zac Hatfield-Dodds <zac@zhd.dev>',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    install_requires=[
+        'typing-extensions>=4.0.0; python_version < "3.9"',
+    ],
+    packages=[
+        'annotated_types',
+    ],
+)
